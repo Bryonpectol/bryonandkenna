fb8db09aa7338dc2d784d2a4e6a7f6e9d992c1cd
Bryon <bryon.pectol@gmail.com>
2023-01-19 05:22:57 -0700

    fixed form

--- js/password.js
+++ js/password.js
@@ -10,10 +10,10 @@ if(localStorage.getItem("key") == "good") {
     Header.style.visibility = "visible";
     Main.style.visibility = "visible";
     Footer.style.visibility = "visible";
-    // btn.style.visibility = "hidden";
-    // btn.setAttribute("height", "0px");
-    // Form.style.visibility = "hidden";
-    // Form.setAttribute("height", "0px");
+    btn.style.visibility = "hidden";
+    btn.setAttribute("height", "0px");
+    Form.style.visibility = "hidden";
+    Form.setAttribute("height", "0px");
 }
 else {
     Header.style.visibility = "hidden";
