2da4c42b324e228f9c9078a19cb56e3ef3fce374
Bryon <bryon.pectol@gmail.com>
2023-01-19 05:15:40 -0700

    added password

---
+++ js/password.js
@@ -0,0 +1,39 @@
+localStorage.getItem("key");
+
+let Header = document.getElementById("header");
+let Main = document.getElementById("main");
+let Footer = document.getElementById("footer");
+let btn = document.getElementById("submit");
+let Form = document.getElementById("form");
+
+if(localStorage.getItem("key") == "good") {
+    Header.style.visibility = "visible";
+    Main.style.visibility = "visible";
+    Footer.style.visibility = "visible";
+    btn.style.visibility = "hidden";
+    btn.setAttribute("height", "0px");
+    Form.style.visibility = "hidden";
+    Form.setAttribute("height", "0px");
+}
+else {
+    Header.style.visibility = "hidden";
+    Main.style.visibility = "hidden";
+    Footer.style.visibility = "hidden";
+}
+
+let Input = document.getElementById("input");
+
+function pass() {
+    if(Input.value == "MaKenna.30") {
+        localStorage.setItem("key", "good");
+        location.reload();
+        console.log("passed!");
+        
+    }
+    
+    if(Input !== "MaKenna.30") {
+        localStorage.setItem("key", "bad");
+        console.log("did not pass :(");
+    }
+    
+}
